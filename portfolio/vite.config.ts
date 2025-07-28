336fedcfbab2d0e04faf3d6904de3dec38c48bdf
wildbergerlopezk <businesswildberger@gmail.com>
2025-07-28 20:10:54 -0300

    probando

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // <- Añade esta línea para que las rutas sean relativas
   plugins: [react()],
   server: {
     port: 5173,
     host: true,
-    allowedHosts: true,  // Permitir todos los hosts (booleano true)
+    allowedHosts: true,
   },
 })
